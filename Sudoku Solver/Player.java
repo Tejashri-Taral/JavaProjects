60775bbe002268001ff0e75eb93300b132638859
Tejashri Taral <160386036+Tejashri-Taral@users.noreply.github.com>
2024-05-11 23:50:55 +0530

    Create Player.java

---
+++ Sudoku Solver/Player.java
@@ -0,0 +1,20 @@
+public class Player {
+    private String name;
+    private int age;
+    
+
+    public Player(String name,int age) {
+        this.name=name;
+        this.age=age;
+        
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+}
