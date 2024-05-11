a1255b832b0c0e627799fe4c51e6350008152614
Tejashri Taral <160386036+Tejashri-Taral@users.noreply.github.com>
2024-05-11 23:51:26 +0530

    Create Triplet.java

---
+++ Sudoku Solver/Triplet.java
@@ -0,0 +1,12 @@
+public class Triplet {
+    int row;
+    int col;
+    int val;
+
+    public Triplet(int row,int col,int val) {
+        this.row=row;
+        this.col=col;
+        this.val=val;
+    }
+
+}
